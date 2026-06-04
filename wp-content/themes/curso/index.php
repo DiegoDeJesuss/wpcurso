3050674eb097927dfe30a6be4ece5d4e2098978d
DiegoDeJesuss <diegodejesuspaula@hotmail.com>
2026-06-04 18:29:21 -0300

    Estrura htlm pronta

--- wp-content/themes/curso/index.php
+++ wp-content/themes/curso/index.php
@@ -5,7 +5,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Curso WordPress</title>
 </head>
-<body>
+<body  <?php  body_class(); ?>>
     <p>Hola Mundo</p>
+    <header>
+        <section class="top-bar">
+            <div class="social-media-icons"></div>
+            <div class="search"></div>
+        </section>
+            <section class="menu-area">
+                <section class="menu-area">
+                    <section class="logo"></section>
+                    <nav class="menu"></nav>
+                </section>
+            </section>
+    </header>
+    <div class="content-area">
+        <main class="site-main">
+            <section class="slide"></section>
+            <section class="services"></section>
+            <section class="middle-area">
+                <aside class="sidebar"></aside>
+                <div class="news"></div>
+            </section>
+            <section class="map"></section>
+        </main>
+        <footer>
+            
+        </footer>
+        
 </body>
 </html>
