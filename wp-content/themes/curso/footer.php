6eb75b0338634ba5156f1d45ad9a14a083226537
DiegoDeJesuss <diegodejesuspaula@hotmail.com>
2026-06-04 19:16:40 -0300

    agora podemos usar o CSS

--- wp-content/themes/curso/footer.php
+++ wp-content/themes/curso/footer.php
@@ -1,6 +1,6 @@
 <footer>
-
-        </footer>
+    <p>&copy; <?php echo date('Y'); ?> Curso WordPress. Todos os direitos reservados.</p>
+</footer>
         
 </body>
 </html>
--- wp-content/themes/curso/header.php
+++ wp-content/themes/curso/header.php
@@ -4,18 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Curso WordPress</title>
+    <?php wp_head(); ?>
 </head>
 <body  <?php  body_class(); ?>>
-    <p>Hola Mundo</p>
+    
     <header>
         <section class="top-bar">
-            <div class="social-media-icons"></div>
-            <div class="search"></div>
+            <div class="social-media-icons">Icones Sociais</div>
+            <div class="search">Pesquisa</div>
         </section>
-            <section class="menu-area">
-                <section class="menu-area">
-                    <section class="logo"></section>
+            <section class="menu-area">       
+                    <section class="logo">Logo</section>
                     <nav class="menu"></nav>
-                </section>
             </section>
     </header>
